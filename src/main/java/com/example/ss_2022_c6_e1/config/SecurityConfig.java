2d0c564f7bd4972c96487c6a8a77e6f17a354bb5
ibrahimmohammedvis@gmail.com <ibrahimmohammedvis@gmail.com>
2023-04-10 11:40:40 -0700

    added comments to the code

--- src/main/java/com/example/ss_2022_c6_e1/config/SecurityConfig.java
+++ src/main/java/com/example/ss_2022_c6_e1/config/SecurityConfig.java
@@ -14,37 +14,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    // Define a bean for PasswordEncoder
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-
     }
 
-
+    // Define a bean for UserDetailsService that will load user details from in-memory storage
     @Bean
     public UserDetailsService userDetailsService() {
         var uds = new InMemoryUserDetailsManager();
 
+        // Define two users, one with "write" authority and the other with "read" authority
         var user1 = User.withUsername("mohammed").password(passwordEncoder().encode("1234")).authorities("write").build();
-
         var user2 = User.withUsername("ibrahim").password(passwordEncoder().encode("1234")).authorities("read").build();
 
+        // Add both users to the in-memory storage
         uds.createUser(user1);
         uds.createUser(user2);
 
         return uds;
     }
 
-
+    // Define a filter chain that handles HTTP security
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.httpBasic().and().authorizeRequests()
-                //.mvcMatchers("/demo/**").hasAnyAuthority("read")//this means all endpoints that comes after /demo/ must have the authority "read" before they can access the endpoint. eg /demo/test1/anything or /demo/test2/anything
-                //.mvcMatchers("/test1").authenticated()
-                //.mvcMatchers("/test2").hasAnyAuthority("re ad")
-                .mvcMatchers(HttpMethod.GET, "/demo/**").hasAnyAuthority("read")//here the same rule is applied but for only GET requests
-                .anyRequest().authenticated().and().csrf().disable()//Don't Do this in real app because you anc introduce vulnurability in your app
-
+                //.mvcMatchers("/demo/**").hasAnyAuthority("read") // Require "read" authority for all endpoints that come after "/demo/"
+                //.mvcMatchers("/test1").authenticated() // Require authentication for "/test1" endpoint
+                //.mvcMatchers("/test2").hasAnyAuthority("re ad") // Require "re ad" authority for "/test2" endpoint
+                .mvcMatchers(HttpMethod.GET, "/demo/**").hasAnyAuthority("read") // Require "read" authority for all GET requests that come after "/demo/"
+                .anyRequest().authenticated() // Require authentication for all other requests
+                .and().csrf().disable() // Disable CSRF protection (for demonstration purposes only, do not use in production code)
                 .build();
     }
 }
