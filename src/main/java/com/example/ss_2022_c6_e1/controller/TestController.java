6679674d5c6895b41250c9bff849e3257fe86e1c
ibrahimmohammedvis@gmail.com <ibrahimmohammedvis@gmail.com>
2023-04-10 11:03:50 -0700

    Configured the security for specific method types to access various endpoints

---
+++ src/main/java/com/example/ss_2022_c6_e1/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.ss_2022_c6_e1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")//this means all the requests in this controller proceeds to the application with the
+public class TestController {
+
+    @GetMapping("/test1")
+    public String test1(){
+        return "test1";
+    }
+
+    @GetMapping("/test2")
+    public String test2(){
+        return "test2";
+    }
+}
